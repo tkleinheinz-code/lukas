a132d2a6f7236f5a71d24f40b086eb7bcbe5609d
tkleinheinz-code <tkleinheinz@gmail.com>
2026-05-18 13:58:31 -0700

    Refactor service worker caching logic

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,15 @@
 const CACHE_NAME = "workout-app-v1";
 
-const urlsToCache = [
-  "/",
-  "index.html",
-  "manifest.json"
-];
-
-// Install
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(["./"]);
+    })
   );
 });
 
-// Fetch (offline support)
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
